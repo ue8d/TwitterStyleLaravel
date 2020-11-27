e107727737d7c7a7a33a90fe4c5e7d0e111b4897
ue8d <webmaster@tomoki12.com>
2020-11-27 23:55:21 +0900

    [add]コメント機能の追加

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,7 @@ Route::group(['middleware' => 'auth'], function() {
 
     // ツイート関連
     Route::resource('tweets', 'TweetsController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+
+    // コメント関連
+    Route::resource('comments', 'CommentsController', ['only' => ['store']]);
 });
